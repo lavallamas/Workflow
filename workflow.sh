a1d93a0d081407d3776426450ac2922d086bbb8b
lavallamas <122215850+lavallamas@users.noreply.github.com>
2023-03-16 15:37:24 -0400

    Added more websites and additional programs to be launched

--- workflow.sh
+++ workflow.sh
@@ -1,4 +1,7 @@
+xdg-open https://google.com
 xdg-open https://pomofocus.io/app
-xdg-open https://github.com/
+xdg-open https://github.com
 xdg-open https://www.theodinproject.com/dashboard
-xdg-open https://music.youtube.com/
+xdg-open https://music.youtube.com
+/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/codium --file-forwarding com.vscodium.codium --unity-launch @@ %F @@
+gnome-terminal
